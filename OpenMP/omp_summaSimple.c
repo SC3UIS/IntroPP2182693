ec13c9de8cfdc82e376b12736525ccca13b9376d
Helman Andres Merchan Quevedo <93000699+GEARSopsDXD12@users.noreply.github.com>
2023-04-24 10:23:24 -0500

    Create omp_summaSimple.c

---
+++ OpenMP/omp_summaSimple.c
@@ -0,0 +1,22 @@
+Summa
+
+#include <stdio.h>
+#include <omp.h>
+
+int main()
+{
+    int num, count, sum = 0;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    #pragma omp parallel for reduction(+:sum)
+    for(count = 1; count <= num; ++count)
+    {
+        sum += count;
+    }
+
+    printf("\nSum = %d\n", sum);
+
+    return 0;
+}
